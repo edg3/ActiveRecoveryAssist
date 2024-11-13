e4ec3af9a710f310814e5eedb0fb8dfa04da1ca7
edg3 <ernest@edg3.co.za>
2024-11-13 21:08:32 +0200

    added asking a question

--- 02.UnoPlatform/ActiveRecoveryAssist/ActiveRecoveryAssist/Presentation/AskViewModel.cs
+++ 02.UnoPlatform/ActiveRecoveryAssist/ActiveRecoveryAssist/Presentation/AskViewModel.cs
@@ -1,7 +1,12 @@
+using ActiveRecoveryAssist.Services.Azure;
+
 namespace ActiveRecoveryAssist.Presentation;
 public partial class AskViewModel : ObservableObject
 {
     private INavigator _navigator;
+
+    [ObservableProperty]
+    private string question = string.Empty;
 
     public AskViewModel(
         IStringLocalizer localizer,
@@ -9,5 +14,19 @@ public partial class AskViewModel : ObservableObject
         INavigator navigator)
     {
         _navigator = navigator;
+
+        Ask = new RelayCommand(async () => await AskCommand());
+    }
+
+    public ICommand Ask { get; }
+
+    private async Task AskCommand()
+    {
+        // Add the question to the database
+        ADB.Add(new Question { Text = Question });
+        ADB.Save();
+
+        // Navigate back to the main page
+        await _navigator.GoBack(this);
     }
 }
