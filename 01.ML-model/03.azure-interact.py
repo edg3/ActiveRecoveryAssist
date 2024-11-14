885dd8b276215648f9c8fa98dfb2857578a39738
edg3 <loveland.ernest@gmail.com>
2024-11-14 05:54:56 +0200

    added base py azure connection to put ML model onto

---
+++ 01.ML-model/03.azure-interact.py
@@ -0,0 +1,26 @@
+# Retrieved by copilot on Bing, prompt: python connect to azure database
+import pyodbc
+
+server = '<your_server>.database.windows.net'
+database = '<your_database>'
+username = '<your_username>'
+password = '<your_password>'
+driver = '{ODBC Driver 17 for SQL Server}'
+
+connection_string = f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}'
+
+# Connect
+conn = pyodbc.connect(connection_string)
+cursor = conn.cursor()
+
+# Get 1 question row
+while True: # TODO: consider a better way to handle this
+    cursor.execute("SELECT TOP 1 * FROM [dbo].[Question] WHERE Answer IS NULL OR Answer = '';")
+    for row in cursor.fetchall():
+        print(row)
+        # TODO: send query to ML model
+        # TODO: send update answer text to Azure row as update
+
+# Close connection
+cursor.close()
+conn.close()
--- 01.ML-model/README.md
+++ 01.ML-model/README.md
@@ -3,3 +3,4 @@
 [X] 02 - Take snippets from guide to get a model to talk to
 [ ] 03 - Make a python auto-refresh check Azure DB for oldest 'new row' every 60 seconds (since POC) and when receives new row to check asks question using prompt added in front:
  - "In the friendliest, most helpful manner, without any agressive stance on any answer, ever, please assist in a positive outlook at life manner: "
+ - Needs: pip install pyodbc
